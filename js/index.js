5a5ba75ca2a78bba3c82b8cdca6d5eaba3d33b82
Michey-Mouse <michelepio44@gmail.com>
2026-02-02 14:28:40 +0100

    .

--- js/index.js
+++ js/index.js
@@ -1,13 +1,18 @@
+//prendiamo l'oggetto div di classe "theme-icon"
 let title = document.getElementsByClassName("theme-icon");
+//prendiamo il valore della variabile "darkmode" dallo storage del browser
 let darkMode = localStorage.getItem('darkMode'); 
+//prendiamo il bottone che usiamo per triggerare il cambiamento tra tema chiaro e tema scuro
 const themeToggleButton = document.getElementById('theme-button');
 
+//funzione per abilitare il tema scuro
 const enableDarkMode = () => {
     title.item(0).setAttribute("title", "Cambia al tema Scuro");
     document.body.classList.add('dark-mode');
     localStorage.setItem('darkMode', 'active');
 }
 
+//funzione per disabilitare il tema scuro
 const disableDarkMode = () => {
     title.item(0).setAttribute("title", "Cambia al tema Chiaro");
     document.body.classList.remove('dark-mode');
@@ -17,6 +22,7 @@ if (darkMode === 'active') {
     enableDarkMode();
 }
 
+//associamo al bottone la funzione per triggerare il cambiamento tra tema chiaro e tema scuro
 themeToggleButton.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode'); 
     if (darkMode !== 'active') {
--- js/sign.js
+++ js/sign.js
@@ -1,13 +1,16 @@
+//prendiamo l'oggetto div di classe "theme-icon"
 let title = document.getElementsByClassName("theme-icon");
-let darkMode = localStorage.getItem('darkMode'); 
+//prendiamo il valore della variabile "darkmode" dallo storage del browser
+let darkMode = localStorage.getItem('darkMode');
+//prendiamo il bottone che usiamo per triggerare il cambiamento tra tema chiaro e tema scuro
 const themeToggleButton = document.getElementById('theme-button');
-
+//funzione per abilitare il tema scuro
 const enableDarkMode = () => {
     title.item(0).setAttribute("title", "Cambia al tema Scuro");
     document.body.classList.add('dark-mode');
     localStorage.setItem('darkMode', 'active');
 }
-
+//funzione per disabilitare il tema scuro
 const disableDarkMode = () => {
     title.item(0).setAttribute("title", "Cambia al tema Chiaro");
     document.body.classList.remove('dark-mode');
@@ -48,7 +51,7 @@ nomed = a[1];
 nomeInput.value = nomep+nomed;
 }
 }
-
+//associamo al bottone la funzione per triggerare il cambiamento tra tema chiaro e tema scuro
 themeToggleButton.addEventListener('click', () => {
     darkMode = localStorage.getItem('darkMode'); 
     if (darkMode !== 'active') {
